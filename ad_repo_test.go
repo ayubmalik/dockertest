eb83be6fe6f8bd7d031b9046d520954008e38d3c
Ayub Malik <ayub.malik@gmail.com>
2021-03-04 19:51:35 +0000

    Remove flag and default to dockertest

--- ad_repo_test.go
+++ ad_repo_test.go
@@ -2,7 +2,6 @@ package dockertestspike_test
 
 import (
 	"database/sql"
-	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -19,22 +18,15 @@ import (
 	dts "github.com/ayubmalik/dockertestspike"
 )
 
-// use docketest by passing -dt flag e.g. go test -dt
-var dt = flag.Bool("dt", false, "Use dockertest container rather than external postgres")
-
 var pool *dockertest.Pool
 
 func TestMain(m *testing.M) {
-	var err error
-	flag.Parse()
-	if *dt {
-		fmt.Println("Using docker test container")
-		pool, err = dockertest.NewPool("")
-		if err != nil {
-			panic(err)
-		}
+	p, err := dockertest.NewPool("")
+	if err != nil {
+		panic(err)
 	}
 
+	pool = p
 	os.Exit(m.Run())
 }
 
@@ -118,48 +110,46 @@ func TestAdRepoFindAll(t *testing.T) {
 func openDB(t *testing.T) *sql.DB {
 	port := "5432"
 
-	if *dt {
-		resource, err := pool.RunWithOptions(&dockertest.RunOptions{
-			Repository: "postgres",
-			Tag:        "13.2-alpine",
-			Env: []string{
-				"POSTGRES_USER=postgres",
-				"POSTGRES_PASSWORD=password",
-				"POSTGRES_DB=dockertest",
-			},
-			ExposedPorts: []string{port},
-		}, func(config *docker.HostConfig) {
-			config.AutoRemove = true
-			config.RestartPolicy = docker.RestartPolicy{
-				Name: "no",
-			}
-		})
-
-		if err != nil {
-			t.Fatalf("Could not start resource: %s", err)
+	resource, err := pool.RunWithOptions(&dockertest.RunOptions{
+		Repository: "postgres",
+		Tag:        "13.2-alpine",
+		Env: []string{
+			"POSTGRES_USER=postgres",
+			"POSTGRES_PASSWORD=password",
+			"POSTGRES_DB=dockertest",
+		},
+		ExposedPorts: []string{port},
+	}, func(config *docker.HostConfig) {
+		config.AutoRemove = true
+		config.RestartPolicy = docker.RestartPolicy{
+			Name: "no",
 		}
+	})
 
-		t.Cleanup(func() {
-			err := pool.Purge(resource)
-			if err != nil {
-				t.Logf("Could not purge resource: %s", err)
-			}
-		})
-
-		port = resource.GetPort(fmt.Sprintf("%s/tcp", port))
-		if err := pool.Retry(func() error {
-			_db, _err := sql.Open("postgres", dsn(port))
-			if _err != nil {
-				return _err
-			}
-			defer _db.Close()
-			return _db.Ping()
-		}); err != nil {
-			log.Fatalf("Could not connect to docker/postgres after retry %s", err)
-		}
-
-		fmt.Println("postgres ready on port:", port)
+	if err != nil {
+		t.Fatalf("Could not start resource: %s", err)
 	}
+
+	t.Cleanup(func() {
+		err := pool.Purge(resource)
+		if err != nil {
+			t.Logf("Could not purge resource: %s", err)
+		}
+	})
+
+	port = resource.GetPort(fmt.Sprintf("%s/tcp", port))
+	if err := pool.Retry(func() error {
+		_db, _err := sql.Open("postgres", dsn(port))
+		if _err != nil {
+			return _err
+		}
+		defer _db.Close()
+		return _db.Ping()
+	}); err != nil {
+		log.Fatalf("Could not connect to docker/postgres after retry %s", err)
+	}
+
+	fmt.Println("postgres ready on port:", port)
 
 	db, err := sql.Open("postgres", dsn(port))
 	if err != nil {
